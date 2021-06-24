1b32583b6245cb1e2e37b4a4028f90f8fcccbb8e
tuzi1412 <tuzi1412@126.com>
2021-06-24 21:39:57 +0800

    init

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+	var input []int = []int{100000, 5, 7, 9, 44, 33, 4, 6, 882222226, 453, 244, 234}
+	QuickSort(input)
+	fmt.Println(input)
+}
+
+func QuickSort(input []int) {
+	sort(0, len(input)-1, input)
+}
+
+func sort(start, end int, input []int) {
+	if start >= end {
+		return
+	}
+	flag := input[start]
+	i := start
+	j := end
+	for i < j {
+		for input[j] >= flag && i < j {
+			j--
+		}
+		for input[i] <= flag && i < j {
+			i++
+		}
+		if i < j {
+			temp := input[i]
+			input[i] = input[j]
+			input[j] = temp
+		}
+	}
+	input[start] = input[i]
+	input[i] = flag
+	sort(start, i-1, input)
+	sort(i+1, end, input)
+}
